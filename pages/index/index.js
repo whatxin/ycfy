24302cece2e9479fbc4bb77970b6eee24dcd6f61
whatxin <2045513674@qq.com>
2019-09-17 13:53:56 +0800

    first

---
+++ pages/index/index.js
@@ -0,0 +1,14 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+    data: {
+        nav: [
+            { id: 1, name: '推荐' },
+            { id: 2, name: '职位' },
+            { id: 3, name: '薪资' },
+            { id: 4, name: '工作经验' }
+        ]
+    }
+})
